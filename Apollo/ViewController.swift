42351c66d9159e87ecc0b57f5a53af87e311b601
Joana Carvalho <joanaisabel1206@gmail.com>
2020-07-14 12:07:51 -0400

    trying to say hello world

---
+++ Apollo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Apollo
+//
+//  Created by Joana Carvalho on 7/13/20.
+//  Copyright © 2020 Joana Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diagonalFront: UIImageView!
+    
+    @IBOutlet weak var apolloTitle: UILabel!
+    
+    @IBOutlet weak var helloWorld: UIView!
+    
+    
+    @IBOutlet weak var apolloSymbol1: UIImageView!
+    
+    @IBOutlet weak var authorNames: UILabel!
+    
+    @IBAction func getStarted(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
