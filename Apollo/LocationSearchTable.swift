1051b5878de834cb8e5e032a60f33c519db50b06
suakari2 <vierakarolina4@gmail.com>
2020-07-15 11:38:57 -0400

    k

---
+++ Apollo/LocationSearchTable.swift
@@ -0,0 +1,14 @@
+//
+//  LocationSearchTable.swift
+//  Apollo
+//
+//  Created by Karolina Viera on 7/15/20.
+//  Copyright © 2020 Joana Carvalho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class LocationSearchTable : UITableViewController {
+
+}
+
