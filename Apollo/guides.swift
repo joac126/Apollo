9e5f22b713c05455f8a14c41961c1e432aabc02c
suakari2 <vierakarolina4@gmail.com>
2020-07-16 15:21:32 -0400

    k

--- Apollo/guides.swift
+++ Apollo/guides.swift
@@ -11,15 +11,30 @@ import UIKit
 class guides: UIViewController {
 
     
+
     @IBOutlet weak var anx: UIImageView!
     
     @IBAction func anxie(_ sender: UIButton) {
         anx.isHidden = false
     }
     
+    
+    @IBOutlet weak var dep: UIImageView!
+    
+    @IBAction func depr(_ sender: UIButton) {
+        dep.isHidden = false}
+    
+    
+    @IBOutlet weak var las: UIImageView!
+    
+    @IBAction func last(_ sender: UIButton) {
+        las.isHidden = false}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         anx.isHidden = true
+        dep.isHidden = true
+        las.isHidden = true
     
         // Do any additional setup after loading the view.
     }
